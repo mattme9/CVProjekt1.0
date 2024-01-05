905a8b72ee1956e8a23c96b5ce70f1bbdb93c9fe
dkarlsson1996 <dkarlsson1996@gmail.com>
2024-01-05 17:46:31 +0100

    reset

---
+++ ViewModels/ResumeVievModel.cs
@@ -0,0 +1,25 @@
+﻿namespace CVProjekt1._0.ViewModels
+{
+    public class ResumeViewModel
+    {
+        public string Description { get; set; }
+        public List<string> Educations { get; set; }
+        public List<string> Skills { get; set; }
+        public List<string> Experiences { get; set; }
+        public List<SkillViewModel> SkillDetails { get; set; }
+        public List<ExperienceViewModel> ExperienceDetails { get; set; }
+    }
+
+    
+    public class SkillViewModel
+    {
+        public string SkillName { get; set; }
+        public string SkillDescription { get; set; }
+    }
+
+ 
+    public class ExperienceViewModel
+    {
+        public string ExperienceDescription { get; set; }
+    }
+}
