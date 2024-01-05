ff86a455e98c100b7a9e87cf311ae28abd1d8a08
antonholtback <121024766+antonholtback@users.noreply.github.com>
2024-01-05 01:51:29 +0100

    ProjectController

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,14 @@
+﻿using CVProjekt1._0.Models;
+using CVProjekt1._0.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVProjekt1._0.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
