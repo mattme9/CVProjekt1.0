18807afc04a9c71d81259ab4fb0f51808064899d
mippen3 <mattias.messerer@gmail.com>
2024-01-05 12:39:26 +0100

    view user details + change details + change password

--- ViewModels/UserInfoViewModel.cs
+++ ViewModels/UserInfoViewModel.cs
@@ -2,13 +2,14 @@
 {
     public class UserInfoViewModel
     {
-        public string UserName { get; set; }
-		public string Email { get; set; }
-		public string PhoneNumber { get; set; } 
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-		public string Address { get; set; } 
-		public string City { get; set; }
-		public string Country { get; set; } 
+        public string? UserName { get; set; }
+		public string? Email { get; set; }
+		public string? PhoneNumber { get; set; } 
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+		public string? Address { get; set; } 
+		public string? City { get; set; }
+		public string? Country { get; set; } 
+		public bool isPrivate { get; set; }
     }
 }
