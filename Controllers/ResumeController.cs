fd7cb1e75358d6689a2c5a9925ca8b6561dc826b
dkarlsson1996 <dkarlsson1996@gmail.com>
2024-01-06 15:16:15 +0100

    Fix

--- Controllers/ResumeController.cs
+++ Controllers/ResumeController.cs
@@ -1,53 +1,63 @@
 ﻿using CVProjekt1._0.Models;
+using CVProjekt1._0.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CVProjekt1._0.Controllers
+public class ResumeController : Controller
 {
-    public class ResumeController : Controller
+    private readonly CVContext _context;
+    private readonly UserManager<User> _userManager;
+
+    public ResumeController(CVContext context, UserManager<User> userManager)
     {
+        _context = context;
+        _userManager = userManager;
+    }
 
+    public async Task<IActionResult> Index()
+    {
+        // Hämta aktuell användare
+        var user = await _userManager.GetUserAsync(User);
 
-        private readonly CVContext _context;
-        private readonly UserManager<User> _userManager;
+        // Hämta beskrivningen från databasen baserat på användar-ID
+        var description = _context.Resumes
+            .Where(r => r.UserId == user.Id)
+            .Select(r => r.Description)
+            .FirstOrDefault();
 
-        public ResumeController(CVContext context, UserManager<User> userManager)
+        // Skapa ett ViewModel för att skicka data till vyn
+        var viewModel = new ResumeViewModel
         {
-            _context = context;
-            _userManager = userManager;
-        }
-        public IActionResult Index()
+            Description = description,
+            // ... andra egenskaper du vill visa på sidan
+        };
+
+        return View(viewModel);
+    }
+
+    
+
+
+public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Resume model)
+    {
+        var user = _userManager.GetUserAsync(User).Result;
+
+        model.UserId = user.Id;
+        var resume = new Resume
         {
-            return View();
-        }
+            Description = model.Description,
+            UserId = model.UserId
+        };
 
-        public IActionResult Create()
-        {
-            return View();
-        }
-        [HttpPost]
-        public async Task <IActionResult> Create(Resume model)
-        {
+        _context.Resumes.Add(resume);
+        _context.SaveChanges();
 
-
-            var user = await _userManager.GetUserAsync(User);
-
-             model.UserId = user.Id;
-             var resume = new Resume
-                {
-                 Description = model.Description,
-                 UserId = model.UserId
-
-             };
-
-                
-                _context.Resumes.Add(resume);
-                _context.SaveChanges();
-
-                return RedirectToAction("Index");
-         
-        }
-
-
+        return RedirectToAction("Index");
     }
 }
