f952181b014be6dfcbf4e146dbcdbbd9d7137cc2
antonholtback <121024766+antonholtback@users.noreply.github.com>
2024-01-06 21:39:07 +0100

    Experiences och skit lagras nu

---
+++ Migrations/20240106192434_skillsreference.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CVProjekt1._0.Migrations
+{
+    /// <inheritdoc />
+    public partial class skillsreference : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Resumes_AspNetUsers_UserId",
+                table: "Resumes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Skill_Resumes_ResumeId",
+                table: "Skill");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Resumes_UserId",
+                table: "Resumes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Skill",
+                table: "Skill");
+
+            migrationBuilder.RenameTable(
+                name: "Skill",
+                newName: "Skills");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Skill_ResumeId",
+                table: "Skills",
+                newName: "IX_Skills_ResumeId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Resumes",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Resumes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Skills",
+                table: "Skills",
+                column: "SkillId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resumes_UserId",
+                table: "Resumes",
+                column: "UserId",
+                unique: true,
+                filter: "[UserId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Resumes_AspNetUsers_UserId",
+                table: "Resumes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Skills_Resumes_ResumeId",
+                table: "Skills",
+                column: "ResumeId",
+                principalTable: "Resumes",
+                principalColumn: "ResumeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Resumes_AspNetUsers_UserId",
+                table: "Resumes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Skills_Resumes_ResumeId",
+                table: "Skills");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Resumes_UserId",
+                table: "Resumes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Skills",
+                table: "Skills");
+
+            migrationBuilder.RenameTable(
+                name: "Skills",
+                newName: "Skill");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Skills_ResumeId",
+                table: "Skill",
+                newName: "IX_Skill_ResumeId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Resumes",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Resumes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Skill",
+                table: "Skill",
+                column: "SkillId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resumes_UserId",
+                table: "Resumes",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Resumes_AspNetUsers_UserId",
+                table: "Resumes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Skill_Resumes_ResumeId",
+                table: "Skill",
+                column: "ResumeId",
+                principalTable: "Resumes",
+                principalColumn: "ResumeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
