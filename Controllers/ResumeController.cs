39455a9b41b4b4a8d47a8085471ea67cd9e27fde
dkarlsson1996 <dkarlsson1996@gmail.com>
2024-01-06 15:25:06 +0100

    Case i create tillagda

--- Controllers/ResumeController.cs
+++ Controllers/ResumeController.cs
@@ -35,29 +35,64 @@ public class ResumeController : Controller
         return View(viewModel);
     }
 
-    
 
 
-public IActionResult Create()
+
+    public IActionResult Create()
     {
         return View();
     }
 
     [HttpPost]
-    public IActionResult Create(Resume model)
+    public IActionResult Create(Resume model, string action)
     {
         var user = _userManager.GetUserAsync(User).Result;
 
         model.UserId = user.Id;
-        var resume = new Resume
-        {
-            Description = model.Description,
-            UserId = model.UserId
-        };
 
-        _context.Resumes.Add(resume);
+        switch (action)
+        {
+            case "AddEducation":
+                // Lägg till logik för "AddEducation" knappen här
+                break;
+            case "AddSkill":
+                // Lägg till logik för "AddSkill" knappen här
+                break;
+            case "AddExperience":
+                // Lägg till logik för "AddExperience" knappen här
+                break;
+            case "SaveAboutMe":
+                // Lägg till logik för "SaveAboutMe" knappen här
+
+                // Här antar jag att du vill uppdatera beskrivningen för användaren
+                var existingResume = _context.Resumes.FirstOrDefault(r => r.UserId == user.Id);
+
+                if (existingResume != null)
+                {
+                    existingResume.Description = model.Description;
+                }
+                else
+                {
+                    // Skapa ett nytt Resume-objekt om det inte finns något befintligt
+                    var newResume = new Resume
+                    {
+                        Description = model.Description,
+                        UserId = model.UserId
+                    };
+
+                    _context.Resumes.Add(newResume);
+                }
+
+                break;
+            default:
+                // Lägg till en fallback om ingen matchning görs
+                break;
+        }
+
+        // Gemensam logik som behöver köras oavsett knapptryckning
         _context.SaveChanges();
 
         return RedirectToAction("Index");
     }
+
 }
