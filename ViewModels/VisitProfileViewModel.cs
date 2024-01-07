5f89e290f0f710aaef6f995a5f5b287397a5d1b0
rutino <121025837+rutino@users.noreply.github.com>
2024-01-07 21:16:45 +0100

    Jobbat på profilen

--- ViewModels/VisitProfileViewModel.cs
+++ ViewModels/VisitProfileViewModel.cs
@@ -8,6 +8,9 @@ namespace CVProjekt1._0.ViewModels
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
         public string ProfilePicture { get; set; }
         public Resume Resume { get; set; }
         public List<Project> Projects { get; set; }
