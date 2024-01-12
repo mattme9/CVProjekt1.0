26f80a755f9bf262a5f1092af78147bfd24f8636
rutino <121025837+rutino@users.noreply.github.com>
2024-01-12 10:59:18 +0100

    rev

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<CVContext>(options =>
-    options.UseLazyLoadingProxies(false).UseSqlServer(builder.Configuration.GetConnectionString("CVContext")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CVContext")));
 builder.Services.AddIdentity<User, IdentityRole>()
     .AddEntityFrameworkStores<CVContext>()
     .AddDefaultTokenProviders();
